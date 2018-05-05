588efd5b2e98a462a00370daf66988d3be9d8f54
xueccc <xueqingc@gmail.com>
2018-05-05 10:55:15 -0700

    Get upadated

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -24,7 +24,7 @@ $('#send .submit').submit(app.handleSubmit);
 app.send = function (message) {
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
-    url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+    url: app.server,
     type: 'POST',
     data: JSON.stringify(message),
     contentType: 'application/json',
@@ -40,10 +40,16 @@ app.send = function (message) {
 
 app.fetch = function () {
   $.ajax({
-    url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-    type: 'GET'
+    url: app.server,
+    type: 'GET',
+    //data: undefined,
+    success: function (data) {
+      console.log('chatterbox: Message fetch' + data);
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to fetch message', data);
+    }
   });
-
 };
 
 app.clearMessages = function () {
